68aef3477874b6a69f478abd7b289f1b49bb6bd9
Rohit Yadav <122785331+cwrohit@users.noreply.github.com>
2023-01-24 16:56:39 +0530

    Add files via upload

---
+++ Ifelse2.java
@@ -0,0 +1,17 @@
+class Ifelse2 {
+  public static void main(String[] args)
+ {
+    int number = 10;
+    if (number > 0)
+ {
+      System.out.println("The number is positive.");
+  }
+    
+   
+    else {
+      System.out.println("The number is not positive.");
+    }
+
+    System.out.println("bhadd m jao");
+  }
+}
