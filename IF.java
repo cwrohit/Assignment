39aaec5561a5a131aa54aec2704c9f8ca60525d5
Rohit Yadav <122785331+cwrohit@users.noreply.github.com>
2023-01-24 16:55:02 +0530

    Add files via upload

---
+++ IF.java
@@ -0,0 +1,13 @@
+
+class If {
+	public static void main(String args[])
+	{
+		int i = 10;
+
+		if (i < 15)
+			System.out.println("10 is less than 15");
+
+		System.out.println("Outside if-block");
+		
+	}
+}
