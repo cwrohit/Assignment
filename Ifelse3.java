aa63b04902e89a668a4f3d931b7ff6329df03878
Rohit Yadav <122785331+cwrohit@users.noreply.github.com>
2023-01-24 16:56:59 +0530

    Add files via upload

---
+++ Ifelse3.java
@@ -0,0 +1,23 @@
+public class Ifelse3
+{
+public static void main(String[] abc)
+{
+
+
+int Number=13;  
+
+    if(number==0)
+{  
+
+        System.out.println(Number+" is even number");  
+
+    }
+else
+{  
+
+        System.out.println(Number+" is odd number");  
+
+    }  
+}
+}
+
