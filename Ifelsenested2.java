1e14069a9f1c9ecd114fb40e899ce6bdabd63884
Rohit Yadav <122785331+cwrohit@users.noreply.github.com>
2023-01-24 17:27:01 +0530

    Add files via upload

---
+++ Ifelsenested2.java
@@ -0,0 +1,19 @@
+class Nestedif
+{
+public static void main(String[]abc)
+{
+int num1=2;
+int num2= 5;
+
+if(num1==2)
+{
+if(num2==2)
+{
+System.out.println("wrong statement");
+}
+else
+{
+System.out.println("wrong");
+}
+}
+}
